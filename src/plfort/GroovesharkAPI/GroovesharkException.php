5effd91edbe38a2d73cd6de92a927e34e9feb266
plfort <plfort@cogipix.com>
2014-01-25 14:30:07 +0100

    Fix GroovesharkException Fix path

--- src/plfort/GroovesharkAPI/GroovesharkException.php
+++ src/plfort/GroovesharkAPI/GroovesharkException.php
@@ -3,16 +3,16 @@ namespace plfort\GroovesharkAPI;
 
 class GroovesharkException extends \Exception{
 
-		private $code;
-		private $message;
+		private $gsCode;
+		private $gsMmessage;
 
 		public function __construct($error){
 			if(is_array($error) && isset($error[0])){
 				if(isset($error[0]['code'])){
-					$this->code = $error[0]['code'];
+					$this->gsCode = $error[0]['code'];
 				}
 				if(isset($error[0]['message'])){
-					$this->message = $error[0]['message'];
+					$this->gsMessage = $error[0]['message'];
 				}
 			}else{
 				if(is_string($error)){
@@ -20,5 +20,15 @@ class GroovesharkException extends \Exception{
 				}
 			}
 		}
+
+	public function getGsCode() {
+		return $this->gsCode;
+	}
+	public function getGsMmessage() {
+		return $this->gsMmessage;
+	}
+
+
+
 
 }
